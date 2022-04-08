01fd61ecc5fd71659646cf832198111b66412004
Morteza Aliyari <79801785+MortezaAliyari@users.noreply.github.com>
2022-04-08 20:30:52 +0800

    Add files via upload

---
+++ GazeboHospital/Hospital/build/CMakeFiles/ActorPlugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ActorPlugin.dir/Actor_v1.cc.o"
+  "libActorPlugin.pdb"
+  "libActorPlugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ActorPlugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
